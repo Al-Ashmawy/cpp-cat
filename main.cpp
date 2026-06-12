26643af7bd1ce65a5ce6cfdb3fe6ebb42ab64ced
Al-Ashmawy <ahmaadtaariq6@gmail.com>
2026-06-12 18:15:56 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,167 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <string_view>
+#include <vector>
+#include <span>
+#include <cstring>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+struct Options {
+        vector<fs::path> files {};
+        optional<fs::path> program_name {};
+        bool number {false};
+        bool number_nonblank {false};
+        bool from_stdin {false};
+        bool show_ends {false};
+        bool show_tabs {false};
+        bool squeeze_blank {false};
+        bool show_nonprinting {false};
+};
+
+Options parse_arguments(span<char*> line) {
+        Options args {};
+
+        if (line.size() > 0) {
+                args.program_name = line[0];
+        }
+
+        for (size_t i = 1; i < line.size(); i++) {
+                string_view word = line[i];
+
+                if (word == "-n" || word == "--number") {
+                        args.number = true;
+                }
+                else if (word == "-b" || word == "--number-nonblank") {
+                        args.number_nonblank = true;
+                }
+                else if (word == "-E" || word == "--show-ends") {
+                        args.show_ends = true;
+                }
+                else if (word == "-T" || word == "--show-tabs") {
+                        args.show_tabs = true;
+                }
+                else if (word == "-s" || word == "--squeeze-blank") {
+                        args.squeeze_blank = true;
+                }
+                else if (word == "-v" || word == "--show-nonprinting") {
+                        args.show_nonprinting = true;
+                }
+                else if (word == "-e") {
+                        args.show_nonprinting = true;
+                        args.show_ends = true;
+                }
+                else if (word == "-t") {
+                        args.show_nonprinting = true;
+                        args.show_tabs = true;
+                }
+                else if (word == "-A" || word == "--show-all") {
+                        args.show_nonprinting = true;
+                        args.show_ends = true;
+                        args.show_tabs = true;
+                }
+                else if (word == "-u") {
+                        // useless flag
+                        // for POSIX compatibility.
+                }
+                else if (word == "-") {
+                        args.from_stdin = true;
+                }
+                else {
+                        if (word.starts_with("--")) {
+                                cerr << args.program_name->string() << ": "
+                                        << "unrecognized option "
+                                        << "'" << word << "'" << endl;
+
+                                exit(EXIT_FAILURE);
+                        }
+
+                        args.files.push_back(word);
+                }
+        }
+
+        if (args.number_nonblank) {
+                args.number = false;
+        }
+
+        return args;
+}
+
+void print_stream(istream& stream, const Options& args) {
+        static unsigned long long line_number = 1;
+
+        string line;
+        bool last_printed_is_blank = false;
+        while (getline(stream, line)) {
+
+                bool is_blank = line.size() == 0;
+                if (args.squeeze_blank && is_blank && last_printed_is_blank) {
+                        continue;
+                }
+
+                if (args.number || (args.number_nonblank && !is_blank) ) {
+                        cout << "    " << line_number << "\t";
+                        line_number++;
+                }
+
+                for (char c : line) {
+                        if (c == '\r') {
+                                if (args.show_ends) {
+                                        cout << "^M";
+                                }
+                                continue;
+                        }
+                        if ((c == '\t') && args.show_tabs) {
+                                cout << "^I";
+                                continue;
+                        }
+                        if (args.show_nonprinting) {
+                                // ....
+                        }
+
+                        cout << c;
+                }
+                if (args.show_ends) {
+                        cout << "$";
+                }
+                cout << endl;
+
+                last_printed_is_blank = is_blank;
+        }
+}
+
+void main_program(const Options& args) {
+        if (!args.program_name.has_value()) {
+                cerr << "Program must have a name!\n" << endl;
+                exit(EXIT_FAILURE);
+        }
+
+        for (auto file_path : args.files) {
+
+                ifstream file(file_path);
+                int error = errno;
+                if (!file.is_open()) {
+                        cout << args.program_name.value().string() << ": ";
+                        cout << file_path.string() << ": ";
+                        cout << strerror(error) << endl;
+                        continue;
+                }
+
+                print_stream(file, args);
+        }
+
+        if (args.from_stdin || args.files.size() == 0) {
+                print_stream(cin, args);
+        }
+}
+
+int main(int argc, char** argv) {
+        Options args = parse_arguments(span(argv, argc));
+
+        main_program(args);
+
+        return 0;
+}
